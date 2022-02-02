9f1d84b9791c35438df760cb177825165e6a1b32
RobeSantoro <santoro.robe@gmail.com>
2022-02-02 15:36:07 +0100

    Added State Machine, States and Controller, Struggling for Animations

--- main.js
+++ main.js
@@ -3,7 +3,491 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 
-class BasicWorldDemo {
+class CharacterControllerProxy {
+  constructor(animations) {
+    this._animations = animations;
+  }
+
+  get animations() {
+    return this._animations;
+  }
+}
+
+class CharacterController {
+  constructor(params) {
+    this._Init(params);
+  }
+
+  _Init(params) {
+    this._params = params;
+    this._decceleration = new THREE.Vector3(-0.0005, -0.0001, -5.0);
+    this._acceleration = new THREE.Vector3(1, 0.25, 50.0);
+    this._velocity = new THREE.Vector3(0, 0, 0);
+
+    this._animations = {};
+    this._input = new CharacterControllerIput();
+    this._stateMachine = new CharacterFSM(
+      new CharacterControllerProxy(this._animations));
+
+    this._LoadModelandAnims();
+  }
+
+  _LoadModelandAnims() {
+
+    const DiffuseTexture = new THREE.TextureLoader()
+      .load('./resources/textures/Avatar_Diffuse.jpg', (texture) => {
+        texture.encoding = THREE.sRGBEncoding;
+        texture.needsUpdate = true;
+        texture.flipY = false;
+      });
+
+    const NormalTexture = new THREE.TextureLoader()
+      .load('./resources/textures/Avatar_Normal.jpg', (texture) => {
+        texture.needsUpdate = true;
+        texture.flipY = false;
+      });
+
+    // Draco loader
+    const dracoLoader = new DRACOLoader()
+    dracoLoader.setDecoderPath('./decoder/')
+
+    const gltfLoader = new GLTFLoader()
+    gltfLoader.setDRACOLoader(dracoLoader)
+
+    gltfLoader.load('./resources/models/Avatar.glb', (gltf) => {
+
+      const model = gltf.scene;
+
+      // Cast shadows and set Materials
+      model.traverse((child) => {
+        if (child.isMesh) {
+
+          child.castShadow = true;
+
+          child.material.map = DiffuseTexture;
+          child.material.normalMap = NormalTexture;
+          child.material.metalness = 0.0;
+          //child.material.envMap = envTexture;
+          child.material.envMapIntensity = 0.1;
+          child.material.needsUpdate = true;
+        }
+      });
+
+      ///////////////////////////////////////////////////////////////////////
+      console.log(gltf.scene);
+
+      this._target = gltf.scene;
+      this._params.scene.add(this._target);
+
+      this._mixer = new THREE.AnimationMixer(this._target);
+
+      this._manager = new THREE.LoadingManager();
+      this._manager.onLoad = () => {
+        this._stateMachine.SetState('idle');
+      };
+
+      const _OnLoad = (animName, anim) => {
+        const clip = anim.animations[0];
+        const action = this._mixer.clipAction(clip);
+
+        this._animations[animName] = {
+          clip: clip,
+          action: action,
+        };
+      };
+
+      ///////////////////////////////////////////////////////////////////////
+
+      const loader = new FBXLoader(this._manager);
+      loader.setPath('./resources/zombie/');
+      loader.load('walk.fbx', (a) => { _OnLoad('walk', a); });
+      loader.load('run.fbx', (a) => { _OnLoad('run', a); });
+      loader.load('idle.fbx', (a) => { _OnLoad('idle', a); });
+      loader.load('dance.fbx', (a) => { _OnLoad('dance', a); });
+    });
+  }
+
+  Update(timeInSeconds) {
+    if (!this._target) {
+      return;
+    }
+
+    this._stateMachine.Update(timeInSeconds, this._input);
+
+    const velocity = this._velocity;
+    const frameDecceleration = new THREE.Vector3(
+      velocity.x * this._decceleration.x,
+      velocity.y * this._decceleration.y,
+      velocity.z * this._decceleration.z
+    );
+    frameDecceleration.multiplyScalar(timeInSeconds);
+    frameDecceleration.z = Math.sign(frameDecceleration.z) * Math.min(
+      Math.abs(frameDecceleration.z), Math.abs(velocity.z));
+
+    velocity.add(frameDecceleration);
+
+    const controlObject = this._target;
+    const _Q = new THREE.Quaternion();
+    const _A = new THREE.Vector3();
+    const _R = controlObject.quaternion.clone();
+
+    const acc = this._acceleration.clone();
+    if (this._input._keys.shift) {
+      acc.multiplyScalar(2.0);
+    }
+
+    if (this._stateMachine._currentState.Name == 'dance') {
+      acc.multiplyScalar(0.0);
+    }
+
+    if (this._input._keys.forward) {
+      velocity.z += acc.z * timeInSeconds;
+    }
+    if (this._input._keys.backward) {
+      velocity.z -= acc.z * timeInSeconds;
+    }
+    if (this._input._keys.left) {
+      _A.set(0, 1, 0);
+      _Q.setFromAxisAngle(_A, 4.0 * Math.PI * timeInSeconds * this._acceleration.y);
+      _R.multiply(_Q);
+    }
+    if (this._input._keys.right) {
+      _A.set(0, 1, 0);
+      _Q.setFromAxisAngle(_A, 4.0 * -Math.PI * timeInSeconds * this._acceleration.y);
+      _R.multiply(_Q);
+    }
+
+    controlObject.quaternion.copy(_R);
+
+    const oldPosition = new THREE.Vector3();
+    oldPosition.copy(controlObject.position);
+
+    const forward = new THREE.Vector3(0, 0, 1);
+    forward.applyQuaternion(controlObject.quaternion);
+    forward.normalize();
+
+    const sideways = new THREE.Vector3(1, 0, 0);
+    sideways.applyQuaternion(controlObject.quaternion);
+    sideways.normalize();
+
+    sideways.multiplyScalar(velocity.x * timeInSeconds);
+    forward.multiplyScalar(velocity.z * timeInSeconds);
+
+    controlObject.position.add(forward);
+    controlObject.position.add(sideways);
+
+    oldPosition.copy(controlObject.position);
+
+    if (this._mixer) {
+      this._mixer.update(timeInSeconds);
+    }
+  }
+
+
+
+
+}
+
+class CharacterControllerIput {
+  constructor() {
+    this._Init();
+  }
+
+  _Init() {
+    this._keys = {
+      forward: false,
+      backward: false,
+      left: false,
+      right: false,
+      space: false,
+      shift: false,
+    };
+    document.addEventListener('keydown', (e) => this._onKeyDown(e), false);
+    document.addEventListener('keyup', (e) => this._onKeyUp(e), false);
+  }
+
+  _onKeyDown(event) {
+    switch (event.keyCode) {
+      case 87: // w
+        this._keys.forward = true;
+        break;
+      case 65: // a
+        this._keys.left = true;
+        break;
+      case 83: // s
+        this._keys.backward = true;
+        break;
+      case 68: // d
+        this._keys.right = true;
+        break;
+      case 32: // SPACE
+        this._keys.space = true;
+        break;
+      case 16: // SHIFT
+        this._keys.shift = true;
+        break;
+    }
+  }
+
+  _onKeyUp(event) {
+    switch (event.keyCode) {
+      case 87: // w
+        this._keys.forward = false;
+        break;
+      case 65: // a
+        this._keys.left = false;
+        break;
+      case 83: // s
+        this._keys.backward = false;
+        break;
+      case 68: // d
+        this._keys.right = false;
+        break;
+      case 32: // SPACE
+        this._keys.space = false;
+        break;
+      case 16: // SHIFT
+        this._keys.shift = false;
+        break;
+    }
+  }
+}
+
+class FiniteStateMachine {
+  constructor() {
+    this._states = {};
+    this._currentState = null;
+  }
+
+  _AddState(name, type) {
+    this._states[name] = type;
+  }
+
+  SetState(name) {
+    const prevState = this._currentState;
+
+    if (prevState) {
+      if (prevState.Name == name) {
+        return;
+      }
+      prevState.Exit();
+    }
+
+    const state = new this._states[name](this);
+
+    this._currentState = state;
+    state.Enter(prevState);
+  }
+
+  Update(timeElapsed, input) {
+    if (this._currentState) {
+      this._currentState.Update(timeElapsed, input);
+    }
+  }
+};
+class CharacterFSM extends FiniteStateMachine {
+  constructor(proxy) {
+    super();
+    this._proxy = proxy;
+    this._Init();
+  }
+
+  _Init() {
+    this._AddState('idle', IdleState);
+    this._AddState('walk', WalkState);
+    this._AddState('run', RunState);
+    this._AddState('dance', DanceState);
+  }
+};
+class State {
+  constructor(parent) {
+    this._parent = parent;
+  }
+
+  Enter() { }
+  Exit() { }
+  Update() { }
+};
+class DanceState extends State {
+  constructor(parent) {
+    super(parent);
+
+    this._FinishedCallback = () => {
+      this._Finished();
+    }
+  }
+
+  get Name() {
+    return 'dance';
+  }
+
+  Enter(prevState) {
+    const curAction = this._parent._proxy._animations['dance'].action;
+    const mixer = curAction.getMixer();
+    mixer.addEventListener('finished', this._FinishedCallback);
+
+    if (prevState) {
+      const prevAction = this._parent._proxy._animations[prevState.Name].action;
+
+      curAction.reset();
+      curAction.setLoop(THREE.LoopOnce, 1);
+      curAction.clampWhenFinished = true;
+      curAction.crossFadeFrom(prevAction, 0.2, true);
+      curAction.play();
+    } else {
+      curAction.play();
+    }
+  }
+
+  _Finished() {
+    this._Cleanup();
+    this._parent.SetState('idle');
+  }
+
+  _Cleanup() {
+    const action = this._parent._proxy._animations['dance'].action;
+
+    action.getMixer().removeEventListener('finished', this._CleanupCallback);
+  }
+
+  Exit() {
+    this._Cleanup();
+  }
+
+  Update(_) {
+  }
+};
+class WalkState extends State {
+  constructor(parent) {
+    super(parent);
+  }
+
+  get Name() {
+    return 'walk';
+  }
+
+  Enter(prevState) {
+    const curAction = this._parent._proxy._animations['walk'].action;
+    if (prevState) {
+      const prevAction = this._parent._proxy._animations[prevState.Name].action;
+
+      curAction.enabled = true;
+
+      if (prevState.Name == 'run') {
+        const ratio = curAction.getClip().duration / prevAction.getClip().duration;
+        curAction.time = prevAction.time * ratio;
+      } else {
+        curAction.time = 0.0;
+        curAction.setEffectiveTimeScale(1.0);
+        curAction.setEffectiveWeight(1.0);
+      }
+
+      curAction.crossFadeFrom(prevAction, 0.5, true);
+      curAction.play();
+    } else {
+      curAction.play();
+    }
+  }
+
+  Exit() {
+  }
+
+  Update(timeElapsed, input) {
+    if (input._keys.forward || input._keys.backward) {
+      if (input._keys.shift) {
+        this._parent.SetState('run');
+      }
+      return;
+    }
+
+    this._parent.SetState('idle');
+  }
+};
+class RunState extends State {
+  constructor(parent) {
+    super(parent);
+  }
+
+  get Name() {
+    return 'run';
+  }
+
+  Enter(prevState) {
+    const curAction = this._parent._proxy._animations['run'].action;
+    if (prevState) {
+      const prevAction = this._parent._proxy._animations[prevState.Name].action;
+
+      curAction.enabled = true;
+
+      if (prevState.Name == 'walk') {
+        const ratio = curAction.getClip().duration / prevAction.getClip().duration;
+        curAction.time = prevAction.time * ratio;
+      } else {
+        curAction.time = 0.0;
+        curAction.setEffectiveTimeScale(1.0);
+        curAction.setEffectiveWeight(1.0);
+      }
+
+      curAction.crossFadeFrom(prevAction, 0.5, true);
+      curAction.play();
+    } else {
+      curAction.play();
+    }
+  }
+
+  Exit() {
+  }
+
+  Update(timeElapsed, input) {
+    if (input._keys.forward || input._keys.backward) {
+      if (!input._keys.shift) {
+        this._parent.SetState('walk');
+      }
+      return;
+    }
+
+    this._parent.SetState('idle');
+  }
+};
+class IdleState extends State {
+  constructor(parent) {
+    super(parent);
+  }
+
+  get Name() {
+    return 'idle';
+  }
+
+  Enter(prevState) {
+    const idleAction = this._parent._proxy._animations['idle'].action;
+    if (prevState) {
+      const prevAction = this._parent._proxy._animations[prevState.Name].action;
+      idleAction.time = 0.0;
+      idleAction.enabled = true;
+      idleAction.setEffectiveTimeScale(1.0);
+      idleAction.setEffectiveWeight(1.0);
+      idleAction.crossFadeFrom(prevAction, 0.5, true);
+      idleAction.play();
+    } else {
+      idleAction.play();
+    }
+  }
+
+  Exit() {
+  }
+
+  Update(_, input) {
+    if (input._keys.forward || input._keys.backward) {
+      this._parent.SetState('walk');
+    } else if (input._keys.space) {
+      this._parent.SetState('dance');
+    }
+  }
+};
+
+
+
+class World {
   constructor() {
     this._Initialize();
   }
@@ -44,10 +528,6 @@ class BasicWorldDemo {
     dirLight.shadow.mapsize = new THREE.Vector2(2048, 2048);
     this._scene.add(dirLight);
 
-    // Light helper
-    const helper = new THREE.DirectionalLightHelper(dirLight, 1);
-    this._scene.add(helper);
-
     // Add an ambient light    
     const ambLight = new THREE.AmbientLight(0xffffff, 0.1);
     this._scene.add(ambLight);
@@ -79,8 +559,20 @@ class BasicWorldDemo {
     plane.rotation.x = -Math.PI / 2;
     this._scene.add(plane);
 
-    this._LoadModel(envTexture);
+    this._mixers = [];
+    this._previousRAF = null;
+
+    //this._LoadModel(envTexture);
+    this._LoadAnimatedModel();
     this._RAF();
+  }
+
+  _LoadAnimatedModel() {
+    const params = {
+      camera: this._camera,
+      scene: this._scene,
+    }
+    this._controls = new CharacterController(params);
   }
 
   _LoadModel(envTexture) {
@@ -96,7 +588,6 @@ class BasicWorldDemo {
       .load('./resources/textures/Avatar_Normal.jpg', (texture) => {
         texture.needsUpdate = true;
         texture.flipY = false;
-
       });
 
     // Draco loader
@@ -106,14 +597,14 @@ class BasicWorldDemo {
     const gltfLoader = new GLTFLoader()
     gltfLoader.setDRACOLoader(dracoLoader)
 
-    let loaded = false;
+    this.ModelLoaded = false;
 
     gltfLoader.load('./resources/models/Avatar.glb', (gltf) => {
 
-      console.log(gltf);
-      const model = gltf.scene;      
 
-      // Cast shadows
+      const model = gltf.scene;
+
+      // Cast shadows and set Materials
       model.traverse((child) => {
         if (child.isMesh) {
 
@@ -129,8 +620,11 @@ class BasicWorldDemo {
         }
       });
 
+
+
       // Add the model to the scene
       this._scene.add(gltf.scene);
+      this.ModelLoaded = true;
     });
   }
 
@@ -152,5 +646,5 @@ class BasicWorldDemo {
 let _APP = null;
 
 window.addEventListener('DOMContentLoaded', () => {
-  _APP = new BasicWorldDemo();
+  _APP = new World();
 });
