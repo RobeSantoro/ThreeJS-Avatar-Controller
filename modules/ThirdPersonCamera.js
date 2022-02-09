1b6473d97aafc3eab0a3456a4f9c56ce0ddb5856
Robe Santoro <robe.santoro@live.com>
2022-02-09 14:08:10 +0100

    Created ThirdPersonCamera Module

---
+++ modules/ThirdPersonCamera.js
@@ -0,0 +1,39 @@
+import * as THREE from 'three';
+
+export class ThirdPersonCamera {
+  constructor(params) {
+    this._params = params;
+    this._CharacterCamera = params.camera;
+
+    this._currentPosition = new THREE.Vector3();
+    this._currentLookat = new THREE.Vector3();
+  }
+
+  _CalculateIdealOffset() {
+    const idealOffset = new THREE.Vector3(-0.35, 2.5, -2);
+    idealOffset.applyQuaternion(this._params.target.Rotation);
+    idealOffset.add(this._params.target.Position);
+    return idealOffset;
+  }
+
+  _CalculateIdealLookat() {
+    const idealLookat = new THREE.Vector3(0, 0, 5);
+    idealLookat.applyQuaternion(this._params.target.Rotation);
+    idealLookat.add(this._params.target.Position);
+    return idealLookat;
+  }
+
+  Update(timeElapsed) {
+    const idealOffset = this._CalculateIdealOffset();
+    const idealLookat = this._CalculateIdealLookat();
+
+    const t = 1.0 - Math.pow(0.001, timeElapsed);
+
+    this._currentPosition.lerp(idealOffset, t);
+    this._currentLookat.lerp(idealLookat, t);
+
+    this._CharacterCamera.position.copy(this._currentPosition);
+    this._CharacterCamera.lookAt(this._currentLookat);
+
+  }
+}
