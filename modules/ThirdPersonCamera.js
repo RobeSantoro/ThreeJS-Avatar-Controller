4ac6bed1992b785331a856c1bb47501069f8cccc
Robe Santoro z800 <robe.santoro@live.com>
2022-02-22 22:26:01 +0100

    Created Params Object

--- modules/ThirdPersonCamera.js
+++ modules/ThirdPersonCamera.js
@@ -2,24 +2,27 @@ import * as THREE from 'three';
 
 export class ThirdPersonCamera {
   constructor(params) {
-    this._params = params;
+
     this._CharacterCamera = params.camera;
+    this._target = params.target;   
 
     this._currentPosition = new THREE.Vector3();
     this._currentLookat = new THREE.Vector3();
+
+    
   }
 
   _CalculateIdealOffset() {
     const idealOffset = new THREE.Vector3(-0.35, 2, -2);
-    idealOffset.applyQuaternion(this._params.target.Rotation);
-    idealOffset.add(this._params.target.Position);
+    idealOffset.applyQuaternion(this._target.Rotation);
+    idealOffset.add(this._target.Position);
     return idealOffset;
   }
 
   _CalculateIdealLookat() {
     const idealLookat = new THREE.Vector3(0, 0, 5);
-    idealLookat.applyQuaternion(this._params.target.Rotation);
-    idealLookat.add(this._params.target.Position);
+    idealLookat.applyQuaternion(this._target.Rotation);
+    idealLookat.add(this._target.Position);
     return idealLookat;
   }
 
@@ -30,7 +33,7 @@ export class ThirdPersonCamera {
     const t = 1.0 - Math.pow(0.001, timeElapsed);
 
     this._currentPosition.lerp(idealOffset, t);
-    this._currentLookat.lerp(idealLookat, t);
+    this._currentLookat.lerp(idealLookat, t);  
 
     this._CharacterCamera.position.copy(this._currentPosition);
     this._CharacterCamera.lookAt(this._currentLookat);
